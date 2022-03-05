0e12ddf69e43f9912e411c89cedfb93a08422716
Davis Jacob <93579312+Davisjacob9@users.noreply.github.com>
2022-03-05 04:49:25 +0000

    p1 done

---
+++ p2original.c
@@ -0,0 +1,5 @@
+void input(int *num1, int *den1, int *num2, int *den2){
+  
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3);
+void output(int num1, int den1, int num2, int den2, int num3, int den3); 
